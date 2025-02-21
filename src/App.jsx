836734af97d3f88f84679b93f0d2e1998421d6af
K-jisu <wltn7star@naver.com>
2025-02-21 20:51:59 +0900

    ✨ feat : AuthProvider 구성

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
-import Router from "./shared/Router";
+import AuthProvider from "./context/AuthProvider";
+import Routes from "./shared/Routes";
 
 function App() {
-  return <Router />;
+  return (
+    <AuthProvider>
+      <Routes />
+    </AuthProvider>
+  );
 }
 
 export default App;
