5dadbf6c34601a65427b11f84f154a78e3e6a2a6
K-jisu <wltn7star@naver.com>
2025-02-21 16:32:15 +0900

    feat : homepage layout 구성

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,34 @@
 import React from "react";
+import HomeCard from "../components/Home/HomeCard";
+import HomeConstant from "../components/Home/Constant/HomeConstant";
 
 const Home = () => {
-  return <div>Home1</div>;
+  return (
+    <div className="w-full flex flex-col items-center justify-center bg-gray-100">
+      <h1 className="text-5xl font-bold text-primary-color mb-6">
+        {HomeConstant.HOME_TITLE}
+      </h1>
+      <p className="mb-8 text-lg text-secondary-color">
+        {HomeConstant.HOME_INFO}
+      </p>
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-8 mb-12">
+        {HomeConstant.HOME_MBTI_CARD.map((card) => {
+          return (
+            <HomeCard
+              title={card.MBTI_TEST_TITLE}
+              content={card.MBTI_TEST_INFO}
+            />
+          );
+        })}
+      </div>
+      <button
+        className="inline-block bg-primary-color text-white py-2 px-6 rounded-full hover:bg-background-color transition mb-4 hover:text-[#FF5A5F]"
+        type="button"
+      >
+        {HomeConstant.HOME_BUTTON}
+      </button>
+    </div>
+  );
 };
 
 export default Home;
